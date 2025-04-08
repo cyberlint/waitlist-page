9b47d8325790b3bc72a45ca797108f5412eed0b6
Jorbbey <jorbbey@gmail.com>
2025-04-08 14:01:51 +0100

    fix

--- src/pages/SecondSection.jsx
+++ src/pages/SecondSection.jsx
@@ -1,25 +1,25 @@
-import Slider from 'react-slick';
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import firstImage from '../assets/Course app-pana.png'
+import firstImage from "../assets/Course app-pana.png";
 import secondImage from "../assets/Online learning-cuate.png";
 import thirdImage from "../assets/Online learning-bro.png";
 import fourthImage from "../assets/Online learning-pana.png";
 import { X } from "lucide-react";
-import Form from '../components/Form';
-import { useState } from 'react';
-const SecondSection = ({classesRef}) => {
-const [boxIsOpen, setBoxIsOpen] = useState(false)
-    const settings ={
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false
-    };
+import Form from "../components/Form";
+import { useState } from "react";
+const SecondSection = ({ classesRef }) => {
+  const [boxIsOpen, setBoxIsOpen] = useState(false);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  };
   return (
     <main
       ref={classesRef}
@@ -53,31 +53,32 @@ const [boxIsOpen, setBoxIsOpen] = useState(false)
           are and join the ride.
         </p>
 
-        <button onClick={()=> setBoxIsOpen(true)}
-         className=" bg-amber-300 w-64 h-12 my-10 m-auto rounded-lg px-3 font-semibold text-[#061c2d] shadow-lg cursor-pointer">
+        <button
+          onClick={() => setBoxIsOpen(true)}
+          className=" bg-amber-300 w-64 h-12 my-10 m-auto rounded-lg px-3 font-semibold text-[#061c2d] shadow-lg cursor-pointer"
+        >
           Yes! I'm Interested
         </button>
       </div>
 
-{/* pop up */}
-{boxIsOpen &&  (
- <div className="fixed inset-0 flex items-center justify-center">
-        <div className="bg-white w-[90%] lg:w-1/2  p-6 rounded-lg shadow-lg relative">
-          {/* Close Button */}
-          <button
-            onClick={() => setBoxIsOpen(false)}
-            className="absolute top-3 right-3 text-gray-600 hover:text-gray-900"
-          >
-            <X size={20} className="cursor-pointer" />
-          </button>
+      {/* pop up */}
+      {boxIsOpen && (
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="bg-white w-[90%] lg:w-1/2  p-6 rounded-lg shadow-lg relative">
+            {/* Close Button */}
+            <button
+              onClick={() => setBoxIsOpen(false)}
+              className="absolute top-3 right-3 text-gray-600 hover:text-gray-900"
+            >
+              <X size={20} className="cursor-pointer" />
+            </button>
 
-          <Form />
+            <Form />
+          </div>
         </div>
-      </div>
-)}
-     
+      )}
     </main>
   );
-}
+};
 
-export default SecondSection
+export default SecondSection;
