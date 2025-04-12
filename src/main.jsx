8299b29f5f24893812cc47031a506977a316b3a9
Jorbbey <jorbbey@gmail.com>
2025-04-12 12:49:32 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -21,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/waitlist-page/" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/thankyou" element={<ThankYou />} />
       </Routes>
     </Router>
